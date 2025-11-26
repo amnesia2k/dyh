b6b9c09eb58aa835e4ad875759afadfba34383c6
Olatilewa <tilewaolatoye17@gmail.com>
2025-11-26 17:16:37 +0100

    feat: commmented out netlify plugin for now

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -4,18 +4,18 @@ import { tanstackStart } from '@tanstack/react-start/plugin/vite'
 import viteReact from '@vitejs/plugin-react'
 import viteTsConfigPaths from 'vite-tsconfig-paths'
 import tailwindcss from '@tailwindcss/vite'
-import netlify from '@netlify/vite-plugin-tanstack-start'
+// import netlify from '@netlify/vite-plugin-tanstack-start'
 import { nitro } from 'nitro/vite'
 
-const isDocker = process.env.IN_DOCKER === '1'
-const isCI = process.env.CI === 'true' // IMPORTANT FIX
+// const isDocker = process.env.IN_DOCKER === '1'
+// const isCI = process.env.CI === 'true' // IMPORTANT FIX
 
 export default defineConfig({
   plugins: [
     devtools(),
 
     // disable Netlify plugin on CI + Docker
-    !isDocker && !isCI && netlify(),
+    // !isDocker && !isCI && netlify(),
 
     viteTsConfigPaths({
       projects: ['./tsconfig.json'],
